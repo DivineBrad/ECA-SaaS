55172b434f2b2839b86aa02fdabce0e794c189f7
Adedeji Ajiboye <ajibdeji@gmail.com>
2017-07-07 12:31:07 -0400

    config and product model
    added values field to appsettings.json and created product model

---
+++ ECA/ECA/Models/Product.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ECA.Models
+{
+    
+    public class Product
+    {
+        public int ID { get; set; }
+        public string ProductName { get; set; }
+        public double ProdPrice { get; set; }
+        public int prodQuantity { get; set; }
+        public int ProdCode { get; set; }
+
+        public Product()
+        {
+
+        }
+        public Product(string productName, double prodPrice, int prodQuantity, int prodCode )
+        {
+             
+        }
+
+
+    }
+}
